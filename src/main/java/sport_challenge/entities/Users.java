60a855704eac882e187d22c892b3a2f2389a752f
aschulz1 <arne.schulz@freenet.ag>
2014-11-20 16:06:54 +0100

    initial

---
+++ src/main/java/sport_challenge/entities/Users.java
@@ -0,0 +1,11 @@
+package sport_challenge.entities;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: aschulz1
+ * Date: 20.11.14
+ * Time: 16:06
+ * To change this template use File | Settings | File Templates.
+ */
+public class Users {
+}
